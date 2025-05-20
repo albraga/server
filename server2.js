994b5d9e144133184af5f30da58637d734d88632
albraga <albraga@email.com>
2025-05-20 17:44:41 -0300

    .

--- server2.js
+++ server2.js
@@ -9,14 +9,25 @@ const users = [
 ];
 
 const server = createServer((req, res) => {
-  const urlMETHOD = req.url + req.method;
-  switch (urlMETHOD) {
-    case '/api/usersGET':
-      send(res, users, 200);
-      break;
-    default:
-      send(res, {message: 'not found'}, 404)
-  };
+  const url = req.url;
+  const METHOD = req.method;
+  const urlMETHOD = url + METHOD;
+  const id = url.slice(5);
+
+  if (isNaN(id) || users.filter(user => user.id === parseInt(id)).length === 0) {
+    send(res, { message: 'not found' }, 404)
+  } else {
+    switch (urlMETHOD) {
+      case '/api/usersGET':
+        send(res, users, 200);
+        break;
+      case `/api/${id}GET`:
+        send(res, users.filter(user => user.id === parseInt(id)), 200);
+        break;
+      default:
+        send(res, { message: 'not found' }, 404)
+    };
+  }
 });
 
 const send = (res, data, statusCode) => {
